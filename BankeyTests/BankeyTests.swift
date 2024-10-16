60a48ce58a5d2d3bdc3e7f2369fa4c37ee794d7f
Bruno Madeira <madeirabruno624@gmail.com>
2024-10-16 23:35:35 +0100

    Initial Commit

---
+++ BankeyTests/BankeyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BankeyTests.swift
+//  BankeyTests
+//
+//  Created by Bruno Madeira on 16/10/2024.
+//
+
+import Testing
+@testable import Bankey
+
+struct BankeyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
